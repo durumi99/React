9ecc94d95045faf3fbdfbe93bcc3d1f197c14c45
durumi99 <kms990615@naver.com>
2021-04-17 18:18:41 +0900

    React

---
+++ src/iterationSample.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const IterationSample = () => {
+    const names =['눈시람','얼음','눈','바람'];
+    const nameList = names.map(name =>);
+}
+
+export default IterationSample;
